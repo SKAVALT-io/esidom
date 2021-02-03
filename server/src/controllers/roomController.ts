e9b89c84d0dbb0ba659b4e26993c2580b54ec03b
furtiveJack <tanguytraina@hotmail.fr>
2021-02-03 16:33:03 +0100

    init(server): add template classes

---
+++ server/src/controllers/roomController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import roomService from '../services/roomService';
+import { App } from '../app';
+
+@App.rest('/room')
+class RoomController {
+    @App.get('')
+    async getRooms(req: Request, res: Response): Promise<void> {
+        const result = await roomService.getRooms();
+        res.status(200).send(result);
+        console.log(this);
+    }
+}
+
+export default RoomController;
---
+++ server/src/services/entityService.ts
@@ -0,0 +1,11 @@
+import httpForwarder from '../forwarders/httpForwarder';
+import { socketForwarder } from '../forwarders/socketForwarder';
+import { Entity } from '../types/entity';
+
+class EntityService {
+    async getEntities(): Promise<Entity[]> {
+        return new Promise((res, res) => {});
+    }
+}
+
+export default new EntityService();
