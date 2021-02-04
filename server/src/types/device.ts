54c8d9944da3a77b43a6fd9e16cec30b449b5ec8
furtiveJack <tanguytraina@hotmail.fr>
2021-02-04 15:02:16 +0100

    feat: add HA return types for device and entity

--- server/src/types/device.ts
+++ server/src/types/device.ts
@@ -1,9 +1,25 @@
+/* eslint-disable camelcase */
 import { Entity } from './entity';
 
 export interface Device {
     id: string;
     name: string;
-    type: string;
     model: string;
     entities: Array<Entity>;
+}
+
+export interface HaDevice {
+    id: string;
+    name: string;
+    connections: { connection_type: string, connection_identifier: string }[];
+    identifiers: (string[])[];
+    manufacturer: string;
+    model: string;
+    config_entries: string[];
+    sw_version: string;
+    via_device_id: string;
+    area_id: string;
+    entry_type: string | null; // only "service" or null
+    name_by_user: string;
+    disabled_by: string;
 }
--- server/src/types/entity.ts
+++ server/src/types/entity.ts
@@ -1,8 +1,20 @@
+/* eslint-disable camelcase */
 export interface Entity {
     id: string;
     entityId: string;
     name: string;
     type: string;
     model: string;
-    date: any;
+    data: any[];
+}
+
+export interface HaEntity {
+    config_entry_id: string;
+    device_id: string;
+    area_id: string;
+    disabled_by: string;
+    entity_id: string;
+    name: string;
+    icon: string;
+    platform: string;
 }
