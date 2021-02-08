1ff5188dd571764757f5bdcb69cfc3a34b23c2f4
Kylian <kbohl@etud.u-pem.fr>
2021-02-08 17:19:12 +0100

    feat(Docker): Launch server and client with specified ip, user and password

---
+++ docker_run.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+if(($# != 3))
+then		echo "Usage : $0 <adresse IP:port> <user> <password>"
+		exit 1
+fi
+
+server=$1
+user=$2
+password=$3
+filename="server/.env"
+container_name="esidom"
+image_name="esidom"
+
+echo "baseUrl=${server}" > ${filename}
+echo "user=${user}" >> ${filename}
+echo "password=${password}" >> ${filename}
+
+docker rm -f ${container_name}
+docker run -p 8080:8080 -p 3000:3000 -d --name ${container_name} ${image_name}
