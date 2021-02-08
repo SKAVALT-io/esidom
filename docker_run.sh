8543cb3f25aaac104c4c47652c0b66dff78879e2
Kylian <kbohl@etud.u-pem.fr>
2021-02-08 18:01:47 +0100

    feat(Docker): Add docker build action

--- docker_run.sh
+++ docker_run.sh
@@ -16,5 +16,7 @@ echo "baseUrl=${server}" > ${filename}
 echo "user=${user}" >> ${filename}
 echo "password=${password}" >> ${filename}
 
+docker build -t ${image_name} .
+
 docker rm -f ${container_name}
 docker run -p 8080:8080 -p 3000:3000 -d --name ${container_name} ${image_name}
