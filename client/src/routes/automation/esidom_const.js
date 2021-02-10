2eda5e71e29a3f67356f566778621d5323ede41a
anais-descombes <anais.descombes@gmail.com>
2021-02-10 15:49:55 +0100

    Create esidom_const.js

---
+++ client/src/routes/automation/esidom_const.js
@@ -0,0 +1,29 @@
+import Blockly from 'blockly';
+
+// Catégorie Logic
+Blockly.Msg.HUE_RED = '330';
+
+// Catégorie Déclencheurs
+Blockly.Msg.HUE_ORANGE = '20';
+
+// Catégorie Conditions
+Blockly.Msg.HUE_YELLOW = '65';
+
+// Catégorie Actions
+Blockly.Msg.HUE_GREEN = '120';
+
+// 
+Blockly.Msg.HUE_TURQUOISE = '160';
+
+//
+Blockly.Msg.HUE_LIGHT_BLUE = '210';
+
+// Catégorie Objets
+Blockly.Msg.HUE_DARK_BLUE = '230';
+
+// Bloc automation
+Blockly.Msg.HUE_PURPLE = '260';
+
+// Catégorie Couleur
+Blockly.Msg.HUE_MAUVE = '290';
+
