4b81c54b63a05144f03ecae79cf41b5a9649dae1
anais-descombes <anais.descombes@gmail.com>
2021-02-10 15:49:47 +0100

    feat(blockly): quick fix import color in .svelte file

--- client/src/routes/automation/Blockly.svelte
+++ client/src/routes/automation/Blockly.svelte
@@ -4,6 +4,7 @@
     import { onMount } from 'svelte';
     import './esidom_blocks';
     import './esidom_generator';
+    import './esidom_const';
 
     onMount(() => {
         const toolbox = document.getElementById("toolbox") || undefined;
@@ -51,35 +52,35 @@
 <div id="test">
     <div id="blocklyDiv" style="height: 600px;" />
     <xml id="toolbox" style="display: none">
-        <category name="Logic" colour="300">
+        <category name="Logic" colour={Blockly.Msg.HUE_RED}>
             <block type="controls_if" />
             <block type="logic_compare" />
             <block type="logic_operation" />
             <block type="logic_negate" />
             <block type="logic_boolean" />
         </category>
-        <category name="Declencheurs" colour="100">
+        <category name="Declencheurs" colour={Blockly.Msg.HUE_ORANGE}>
             <block type="binary_trigger"></block>
             <block type="time"></block>
         </category>
-        <category name="Conditions" colour="150">
+        <category name="Conditions" colour={Blockly.Msg.HUE_YELLOW}>
             <block type="time_condition"></block>
             <block type="time_condition_hour"></block>
             <block type="time_condition_week"></block>
             <block type="binary_condition"></block>
         </category>
-        <category name="Actions" colour="200">
-            <category name="Services" colour="200">
+        <category name="Actions" colour={Blockly.Msg.HUE_GREEN}>
+            <category name="Services" colour={Blockly.Msg.HUE_GREEN}>
                 <block type="action"></block>
                 <!-- <block type="services"></block> -->
             </category>
         </category>
-        <category name="Objets" colour="50">
+        <category name="Objets" colour={Blockly.Msg.HUE_DARK_BLUE}>
             <!-- <block type="lamps"></block> -->
             <!-- <block type="binary_sensors"></block> -->
             <!-- <block type="objet_action"></block> -->
         </category>
-        <category name="Couleur" colour="75">
+        <category name="Couleur" colour={Blockly.Msg.HUE_MAUVE}>
             <block type="color_picker"></block>
             <block type="color_rgb"></block>
         </category>
