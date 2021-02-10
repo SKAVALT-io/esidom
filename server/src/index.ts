bad4ca98ee1c4b20e14e224bfe8c5432e99971ce
furtiveJack <tanguytraina@hotmail.fr>
2021-02-10 02:08:27 +0100

    feat(server): authenticate to Ha when started

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { config } from 'dotenv';
+import axios, { AxiosResponse } from 'axios';
 import App from './app';
 import deviceController from './controllers/deviceController';
 import authController from './controllers/authController';
@@ -17,12 +18,22 @@ App.app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Server up and running!');
 });
 
-const port: number = 3000;
-App.http.listen(port, () => {
-    console.log('App is listening on port 3000 !');
+const port: number = 3001;
+App.http.listen(port, async () => {
+    console.log(`App is listening on port ${port} !`);
 });
 
-// Instanciat each controller
+const doAuth = async () => {
+    const { status } = await axios
+        .get(`http://localhost:${port}/auth`) as AxiosResponse<any>;
+    if (status !== 200) {
+        throw new Error('Unable to authenticate server to HA');
+    }
+    console.log('Server authenticated to HA !');
+};
+doAuth();
+
+// 'Instanciate' each controller
 [
     deviceController, authController, entityController,
     roomController, automationController, groupController, userController,
