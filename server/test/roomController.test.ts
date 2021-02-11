9394fe72ea569af24ba53819754b85586767339f
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-11 09:10:13 +0100

    fix(server): fix lint error

--- server/test/roomController.test.ts
+++ server/test/roomController.test.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Room } from '../src/types/room';
 
 const mock: Room = expect.objectContaining({
