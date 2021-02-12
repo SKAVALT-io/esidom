f381e36d1163c5e0bc98151dc576000dfd8a7169
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-12 16:58:13 +0100

    fix(server): fix test in group controller

--- server/src/controllers/groupController.ts
+++ server/src/controllers/groupController.ts
@@ -19,12 +19,12 @@ class GroupController {
             res.status(400).send({ message: 'The parameter name is missing' });
             return;
         }
-        if (entities === undefined || name === []) {
+        if (entities === undefined || entities === []) {
             res.status(400).send({ message: 'The parameter entities is missing' });
             return;
         }
         try {
-            const result = await groupService.createGroup(req.body.name, req.body.entities);
+            const result = await groupService.createGroup(name, entities);
             res.status(200).send(result);
         } catch (err: any) {
             res.status(400).send({ message: err.message ? err.message : err });
