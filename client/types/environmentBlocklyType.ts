02ca4b8a1261bb905f8d64a08094d738ff101f02
Léo OUCH <youheng93160@hotmail.fr>
2021-02-12 12:52:14 +0100

    feat(blockly): add types

---
+++ client/types/environmentBlocklyType.ts
@@ -0,0 +1,11 @@
+export interface EnvironmentBlockly {
+    blockId: string;
+    element: string;
+    group: string;
+    isBlank: boolean;
+    name: string;
+    newValue: string;
+    oldValue: string;
+    recordUndo: boolean;
+    workspaceId: string;
+}
