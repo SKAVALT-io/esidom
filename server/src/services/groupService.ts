10ac00d2a7372fac7e0c87e9ee7db3b62e10e5d2
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-12 17:56:27 +0100

    fix(server): split methode create group

--- server/src/services/groupService.ts
+++ server/src/services/groupService.ts
@@ -40,15 +40,46 @@ class GroupService {
         // Test if group exist
         const g = await databaseForwarder.db?.get<DBGroup>(`SELECT * FROM ${NameGroupTable} WHERE entityId = '${groupEntityId}'`);
         if (g !== undefined) {
-            return Promise.reject(new Error('Group already exist'));
+            throw new Error('Group already exist');
         }
-
         // Get list of entity with full detail
         const entitiesFull = (await entityService.getEntities())
             .filter((value: Entity) => entities.includes(value.id));
 
+        this.checkDataGroupToCreate(entities, entitiesFull);
+        await httpForwarder.post('/api/services/group/set', {
+            object_id: groupEntityId,
+            name: `${name}`,
+            entities: entities.join(','),
+        });
+        // Insert group into ESIDOM DB
+        this.insertGroupIntoDb(groupEntityId, name, entities);
+        const group: Group = {
+            groupId: groupEntityId,
+            name,
+            entities: entitiesFull,
+        };
+        return group;
+    }
+
+    private async insertGroupIntoDb(groupId: string, name: string, entities: string[]) {
+        try {
+            await databaseForwarder.db?.run('BEGIN TRANSACTION');
+            await databaseForwarder.db?.run(`INSERT INTO ${NameGroupTable} (entityId, name) VALUES ('${groupId}','${name}')`);
+            await Promise.all(
+                entities.map(async (entityId: string) => databaseForwarder.db?.run(`INSERT INTO ${NameInsideGroupTable} (entityId, groupEntityId) VALUES ('${entityId}','${groupId}')`)),
+            );
+            await databaseForwarder.db?.run('COMMIT');
+        } catch (err) {
+            await databaseForwarder.db?.run('ROLLBACK');
+            throw err;
+        }
+
+    }
+
+    private checkDataGroupToCreate(entities: string[], entitiesFull: Entity[]) {
         if (entitiesFull === undefined) {
-            return Promise.reject(new Error('All entities not found in HA'));
+            throw new Error('All entities not found in HA');
         }
 
         // Test if each entity supply exist in HA
@@ -57,33 +88,9 @@ class GroupService {
         for (let i = 0; i < entities.length; i++) {
             const e = entities[i];
             if (!entitiesFullOnlyId.includes(e)) {
-                return Promise.reject(new Error(`Entity ${e} doesn't exist`));
+                throw new Error(`Entity ${e} doesn't exist`);
             }
         }
-        await httpForwarder.post('/api/services/group/set', {
-            object_id: groupEntityId,
-            name: `${name}`,
-            entities: entities.join(','),
-        }).catch((err) => { console.log(JSON.stringify(err)); });
-
-        // Insert group into ESIDOM DB
-        try {
-            await databaseForwarder.db?.run('BEGIN TRANSACTION');
-            await databaseForwarder.db?.run(`INSERT INTO ${NameGroupTable} (entityId, name) VALUES ('${groupEntityId}','${name}')`);
-            await Promise.all(
-                entities.map(async (entityId: string) => databaseForwarder.db?.run(`INSERT INTO ${NameInsideGroupTable} (entityId, groupEntityId) VALUES ('${entityId}','${groupEntityId}')`)),
-            );
-            await databaseForwarder.db?.run('COMMIT');
-        } catch (err) {
-            await databaseForwarder.db?.run('ROLLBACK');
-            return Promise.reject(new Error(err));
-        }
-        const group: Group = {
-            groupId: groupEntityId,
-            name,
-            entities: entitiesFull,
-        };
-        return group;
     }
 
     private normalizeEntityId(name: string): string {
@@ -95,13 +102,13 @@ class GroupService {
             console.log(err);
         });
         if (res === undefined) {
-            return Promise.reject(new Error('All entities not found in HA'));
+            throw new Error('All entities not found in HA');
         }
         // For each group
         const groups: Group[] = await Promise.all(res.map(async (g: DBGroup) => {
             const insideGroup = await databaseForwarder.db?.all<InsideGroup[]>(`SELECT * FROM ${NameInsideGroupTable} WHERE groupEntityId = '${g.entityId}'`);
             if (insideGroup === undefined) {
-                return Promise.reject(new Error('Group with no entity'));
+                throw new Error('Group with no entity');
             }
             const entitiesId = insideGroup.map((value: InsideGroup) => value.entityId);
             const entitiesFull = (await entityService.getEntities())
