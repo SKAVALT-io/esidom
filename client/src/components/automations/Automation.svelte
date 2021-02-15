748770c35cd06b3d6c23b189a249143ff57f582b
furtiveJack <tanguytraina@hotmail.fr>
2021-02-15 19:13:01 +0100

    feat(client): add automations listing page

---
+++ client/src/components/automations/Automation.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { push } from 'svelte-spa-router';
+
+    import type { AutomationPreview } from '../../../types/automationType';
+    import RoundedButton from '../UI/buttons/RoundedButton.svelte';
+    import ToggleButton from '../UI/buttons/ToggleButton.svelte';
+
+    export let automation: AutomationPreview;
+    let checked = automation.state === 'on';
+
+    function handleChange(event: { target: HTMLInputElement }) {
+        // TODO: make HTTP request
+    }
+</script>
+
+<div
+    id="automation"
+    class="rounded items-center text-center grid grid-cols-10 px-1 py-4"
+>
+    <div class="col-span-1">
+        <ToggleButton bind:checked />
+    </div>
+    <div class="flex justify-center items-center col-span-8">
+        {automation.name}
+    </div>
+    <div class="col-span-1 relative">
+        <RoundedButton
+            size={8}
+            on:click={() /*TODO: go to edit page*/ => push('/home')}
+            iconPath="icons/edit.svg"
+        />
+    </div>
+</div>
+
+<style>
+    #automation {
+        background-color: #22164d;
+    }
+</style>
---
+++ client/src/routes/automation/Automations.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { AutomationPreview } from '../../../types/automationType';
+    import Automation from '../../components/automations/Automation.svelte';
+    import AutomationService from '../../services/automationService';
+</script>
+
+{#await AutomationService.getAutomations()}
+    <p>loading</p>
+{:then automations}
+    <div
+        id="automation"
+        class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mr-2 ml-2 mt-2"
+    >
+        {#each automations as automation}
+            <Automation {automation} />
+        {/each}
+    </div>
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
