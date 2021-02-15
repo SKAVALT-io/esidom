09f950b1c72691ddb8b746eb73babd26fb994804
furtiveJack <tanguytraina@hotmail.fr>
2021-02-15 19:06:42 +0100

    feat(client): fix RoundedButton position

--- client/src/components/UI/buttons/RoundedButton.svelte
+++ client/src/components/UI/buttons/RoundedButton.svelte
@@ -5,7 +5,7 @@
 
 <button
     on:click
-    class="roundedButton absolute inline-flex items-center justify-center p-2 {`w-${size}`} {`h-${size}`} text-white transition-colors duration-150 bg-blue-500 rounded-full focus:shadow-outline hover:bg-blue-600"
+    class="roundedButton inline-flex items-center justify-center p-2 {`w-${size}`} {`h-${size}`} text-white transition-colors duration-150 bg-blue-500 rounded-full focus:shadow-outline hover:bg-blue-600"
 >
     <img class="w-5 h-5" src={iconPath} alt="" />
 </button>
