b5782d7bccf8971cfd1ec1ed751b9c8074e2f80d
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-17 01:14:30 +0100

    feat(server): add new methodes to observer

--- server/src/types/observer.ts
+++ server/src/types/observer.ts
@@ -7,6 +7,8 @@ export interface EventObserver {
     onAutomationUpdated?: (data: HaEntityUpdated) => void;
     onEntityRegistryUpdated?: () => void;
     onDeviceRegistryUpdated?: () => void;
+    onAreaUpdated?: (roomId: string) => void;
+    onAreaRemoved?: (roomId: string) => void;
 }
 
 export type Event = 'authOk' | 'entityUpdated' | 'automationUpdated' | 'entityRegistryUpdated' | 'deviceRegistryUpdated';
