c506b0e15518fa0b713632333197bed5b11a8d21
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-18 12:26:11 +0100

    feat(front): create group component

---
+++ client/src/components/groups/Group.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { push } from "svelte-spa-router";
+    import { onDestroy, onMount } from "svelte";
+
+    import RoundedButton from "../UI/buttons/RoundedButton.svelte";
+    import ToggleButton from "../UI/buttons/ToggleButton.svelte";
+    import type { Group } from "../../../types/groupType";
+    import Modal from "../../components/UI/modal/Modal.svelte";
+
+    export let group: Group;
+
+    $: checked = true;
+
+    function handleToggle() {
+        checked = !checked;
+    }
+
+    onMount(async () => {});
+
+    onDestroy(() => {});
+</script>
+
+<div
+    id="group"
+    class="rounded items-center text-center grid grid-cols-10 px-1 py-4"
+>
+    <div class="col-span-1">
+        <ToggleButton on:change={handleToggle} bind:checked />
+    </div>
+    <div class="flex justify-center items-center col-span-8">{group.name}</div>
+    {#if !group.groupId.startsWith('imp')}
+        <div class="col-span-1 relative">
+            <RoundedButton
+                size={8}
+                on:click={() /*TODO: go to edit page*/ => push('/home')}
+                iconPath="icons/edit.svg"
+            />
+        </div>
+    {/if}
+</div>
+
+<style>
+    #group {
+        background-color: #22164d;
+    }
+</style>
