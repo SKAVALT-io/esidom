6bae9076fc75afece8bdecce9938a5eb5fa01d8f
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-18 12:26:29 +0100

    feat(front): create groupService

---
+++ client/src/services/groupService.ts
@@ -0,0 +1,15 @@
+import type { Group } from '../../types/groupType';
+import config from '../config/config';
+
+export default class GroupService {
+    static async getGroup(): Promise<Group[]> {
+        const headers = new Headers();
+        headers.set('Content-Type', 'application/json');
+        return fetch(`${config.BASE_URL}/group`, {
+            headers,
+            method: 'GET',
+        })
+            .then((res) => res.json())
+            .catch((err) => console.log(err));
+    }
+}
