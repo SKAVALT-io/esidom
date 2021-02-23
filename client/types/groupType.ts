a3a0686a4489e8993aeb3277da276c6cc964ed7b
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-24 00:27:45 +0100

    feat(front): add new type file

--- client/types/groupType.ts
+++ client/types/groupType.ts
@@ -1,7 +1,12 @@
 import type { Entity } from './entityType';
+import type { Room } from './roomType';
 
 export interface Group {
     groupId?: string;
     name?: string;
-    entities: Entity[];
+    state?: string;
+    entities: Entity<any>[];
+    implicit: boolean;
+    room?: Room;
+    type?:string;
 }
