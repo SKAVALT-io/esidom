7d0b0a06a3151ff55f45bf13d42c316ba0d20d07
anais-descombes <anais.descombes@gmail.com>
2021-02-24 17:09:01 +0100

    feat(front): responsive Light page v1

--- client/src/components/entities/lights/Light.svelte
+++ client/src/components/entities/lights/Light.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onDestroy, SvelteComponent } from 'svelte';
     import { onMount } from 'svelte';
+    import { pop } from 'svelte-spa-router';
 
     import {
         switchLamp,
@@ -13,6 +14,9 @@
     import { socketManager } from '../../../managers/socketManager';
     import type { LightEntity } from '../../../../types/entities/lightEntity';
     import { getEntity } from '../../../services/entityService';
+
+    import ToggleButton from '../../UI/buttons/ToggleButton.svelte';
+    import BorderedButton from '../../UI/buttons/BorderedButton.svelte';
 
     export let entityId: string;
 
@@ -58,30 +62,54 @@
 {#await loadLight()}
     Loading data...
 {:then}
-    <div>
-        <h1>{light.name}</h1>
+    <div id="content" class="px-8">
+        <div id="title">
+            <!-- <button
+                on:click={pop}
+                class="border-white border-2 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+            >Retour</button> -->
+            <h1 class="text-4xl text-center py-6">{light.name}</h1>
+        </div>
 
         <div
             id="info"
-            class="left-1/4 relative w-1/2 grid grid-cols-12 gap-4 p-4 uppercase"
+            class="relative grid grid-cols-12 gap-4 p-4 uppercase bg-esidomlight w-full md:w-4/6 md:left-1/6 max-w-5xl"
         >
-            <div class="col-span-full text-center">Informations</div>
+            <div class="col-span-full text-center font-semibold text-xl">
+                Informations
+            </div>
 
-            <div
+            <!-- <div
                 class="col-span-6 row-start-2 text-center"
                 on:click={() => switchLamp(entityId, !isOn)}
             >
-                {isOn ? 'Éteindre' : 'Allumer'}
-            </div>
-            <div class="col-span-3 row-start-3">Nom:</div>
-            <div class="col-span-3 row-start-3">{light.name}</div>
-            <div class="col-span-3 row-start-4">Type:</div>
-            <div class="col-span-3 row-start-4">{light.type}</div>
-            <div class="col-span-3 row-start-5">Etat:</div>
+                <button
+                    on:click={pop}
+                    class="border-white border-2 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+                >{isOn ? 'Éteindre' : 'Allumer'}</button>
+                <ToggleButton
+                    on:change={() => switchLamp(entityId, !isOn)}
+                    bind:{isOn}
+                />
+            </div> -->
+            <div class="col-span-3 row-start-3 text-right">Nom :</div>
+            <div class="col-span-9 row-start-3">{light.name}</div>
+            <div class="col-span-3 row-start-4 text-right">Type :</div>
+            <div class="col-span-9 row-start-4">{light.type}</div>
+            <div class="col-span-3 row-start-5 text-right">Etat :</div>
             <div class="col-span-3 row-start-5">{light.state}</div>
+            <div class="col-span-3 row-start-5">
+                <ToggleButton
+                    on:change={() => switchLamp(entityId, !isOn)}
+                    bind:isOn
+                />
+            </div>
         </div>
 
-        <div id="attributes" class=" grid grid-cols-1 gap-4 p-4 m-4">
+        <div
+            id="attributes"
+            class="relative grid grid-cols-1 gap-4 p-4 w-full md:w-4/6 md:left-1/6 max-w-5xl"
+        >
             {#if isOn}
                 {#each Object.entries(light.attributes) as [key, value] (key)}
                     {#if lightPropMap.has(key)}
@@ -95,7 +123,9 @@
                         </div>
                     {/if}
                 {/each}
-            {:else}Allumez l'appareil :){/if}
+            {:else}
+                Allumez l'appareil si vous souhaitez configurer la lampe.
+            {/if}
         </div>
     </div>
 {:catch error}
@@ -103,11 +133,11 @@
     {error}
 {/await}
 
-<style lang="scss">
+<!--<style lang="scss">
     #info {
         border: 1px solid yellow;
         div:not(:first-child) {
             border: 1px solid yellow;
         }
     }
-</style>
+</style>-->
