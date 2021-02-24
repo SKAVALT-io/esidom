c473403a492a3248b0c02207f31428f93f3bfc50
furtiveJack <tanguytraina@hotmail.fr>
2021-02-24 10:36:08 +0100

    fix(server): fuck you linter

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 export { default as httpService } from './forwarderServices/httpService';
 export { default as socketService } from './forwarderServices/socketService';
 
