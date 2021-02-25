bd917567ec5cf8a5e34989626a502203e712ad7d
asoran <altan.soran@gmail.com>
2021-02-25 18:54:23 +0100

    fix(client): fix starting item menu

--- client/src/components/UI/bar/Sidebar.svelte
+++ client/src/components/UI/bar/Sidebar.svelte
@@ -4,7 +4,16 @@
     import { clickOutside } from '../../../utils/functions';
     export let open = false;
 
-    let currentPageSelected = 'entities';
+    function hashToPage() {
+        const sub = window.location.hash.substring(2);
+        return sub !== '' ? sub : 'entities';
+    }
+
+    let currentPageSelected = hashToPage();
+    window.onhashchange = () => {
+        currentPageSelected = hashToPage();
+    };
+
     const pageLinkClicked = (currentPage: string) => {
         currentPageSelected = currentPage;
         open = false;
