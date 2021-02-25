7994915ad917e5713c1a42b7d6399b6a01e7365d
Sacha Ouziel <ouziel.sacha@gmail.com>
2021-02-25 12:20:23 +0100

    fix(front): use axios in groupService

--- client/src/services/groupService.ts
+++ client/src/services/groupService.ts
@@ -1,50 +1,35 @@
-import type { Group } from '../../types/groupType';
+import type { Group, NewGroup } from '../../types/groupType';
 import config from '../config/config';
+import HttpHelper from '../utils/HttpHelper';
+import { tr } from '../utils/i18nHelper';
 
 export default class GroupService {
     static async getGroup(): Promise<Group[]> {
-        const headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        return fetch(`${config.BASE_URL}/group`, {
-            headers,
-            method: 'GET',
-        })
-            .then((res) => res.json())
-            .catch((err) => console.log(err));
+        const groups = await HttpHelper.get<Group[]>('/group');
+
+        return groups.map((g) => this.updateGroupNameIfIsImplicit(g));
     }
 
-    static async createGroup(group: Group): Promise<Group> {
-        const headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        return fetch(`${config.BASE_URL}/group`, {
-            headers,
-            method: 'POST',
-            body: JSON.stringify(group),
-        })
-            .then((res) => res.json())
-            .catch((err) => console.log(err));
+    static async createGroup(group: NewGroup): Promise<Group> {
+        return HttpHelper.post('/group', group);
     }
 
     static async deleteGroup(group: Group): Promise<Group> {
-        const headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        return fetch(`${config.BASE_URL}/group/${group.groupId}`, {
-            headers,
-            method: 'DELETE',
-        })
-            .then((res) => res.json())
-            .catch((err) => console.log(err));
+        return HttpHelper.delete(`/group/${group.groupId}`);
     }
 
     static async updateGroup(group: Group): Promise<Group> {
-        const headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        return fetch(`${config.BASE_URL}/group/${group.groupId}`, {
-            headers,
-            method: 'PUT',
-            body: JSON.stringify(group),
-        })
-            .then((res) => res.json())
-            .catch((err) => console.log(err));
+        return HttpHelper.put(`/group/${group.groupId}`, group);
+    }
+
+    static updateGroupNameIfIsImplicit(group: Group): Group {
+        const updatedGroup = group;
+        if (updatedGroup.implicit) {
+            updatedGroup.name = `[${tr('groups.implicitGroup.name')}] ${tr(`groups.implicitGroup.cat.${group.type}`)}`;
+            if (group.room) {
+                updatedGroup.name += `${tr('groups.implicitGroup.of')} ${group.room.name}`;
+            }
+        }
+        return updatedGroup;
     }
 }
