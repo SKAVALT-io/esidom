18f00383e76155ba954e41e4d3d4c149f92eed66
anais-descombes <anais.descombes@gmail.com>
2021-02-25 18:14:36 +0100

    feat(front): adding switch page

---
+++ client/src/components/entities/switchs/Switch.svelte
@@ -0,0 +1,57 @@
+<script>
+    import { onDestroy, SvelteComponent } from 'svelte';
+    import type { SwitchEntity } from '../../../../types/entities/switchEntity';
+
+    import { socketManager } from '../../../managers/socketManager';
+    import { getEntity } from '../../../services/entityService';
+
+    export let entityId: string;
+    let entity: SwitchEntity;
+
+    async function loadEntity() {
+        entity = await getEntity<SwitchEntity>(entityId);
+        console.log('binary sensor: ', entity);
+    }
+
+    function updateEntity(data: SwitchEntity) {
+        console.log('new ws', data);
+        entity = data;
+    }
+
+    socketManager.registerListener('entity_updated', entityId, updateEntity);
+
+    onDestroy(() => {
+        socketManager.removeListener('entity_updated', updateEntity);
+    });
+</script>
+
+{#await loadEntity()}
+    Loading data...
+{:then l}
+    <div>
+        <div id="title">
+            <h1 class="text-4xl text-center py-6">{entity.name}</h1>
+        </div>
+
+        <div id="content" class="flex flex-row justify-center">
+            <div
+                id="info"
+                class="relative grid grid-cols-12 gap-4 py-4 uppercase bg-esidomlight w-full justify-center md:w-2/3 md:max-w-xl"
+            >
+                <div class="col-span-full text-center font-semibold text-xl">
+                    Informations
+                </div>
+
+                <div class="col-span-3 row-start-2 text-right">Nom :</div>
+                <div class="col-span-9 row-start-2">{entity.name}</div>
+                <div class="col-span-3 row-start-3 text-right">Type :</div>
+                <div class="col-span-9 row-start-3">{entity.type}</div>
+                <div class="col-span-3 row-start-4 text-right">Etat :</div>
+                <div class="col-span-9 row-start-4">{entity.state}</div>
+            </div>
+        </div>
+    </div>
+{:catch error}
+    Error:
+    {error}
+{/await}
---
+++ client/src/components/entities/switchs/SwitchPreview.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { onDestroy, onMount } from 'svelte';
+    import type { SwitchEntity } from '../../../../types/entities/switchEntity';
+
+    import { socketManager } from '../../../managers/socketManager';
+    import { tr } from '../../../utils/i18nHelper';
+
+    import EntityPreview from '../EntityPreview.svelte';
+
+    export let entity: SwitchEntity;
+
+    // Is it on or off
+    let isOn: boolean;
+    // The src for the icon
+    let srcImg = 'prise.png';
+    $: isOn = entity.state === 'on';
+
+    function updateState(data: SwitchEntity) {
+        console.log('new ws', data);
+        entity = data;
+    }
+
+    onMount(() => {
+        socketManager.registerListener(
+            'entity_updated',
+            entity.id,
+            updateState
+        );
+    });
+
+    onDestroy(() => {
+        socketManager.removeListener('entity_updated', updateState);
+    });
+</script>
+
+<EntityPreview isError={false} {entity}>
+    <!-- Image -->
+    <img
+        slot="img"
+        src={srcImg}
+        alt=""
+        class="h-inherit max-w-full max-h-full object-contain"
+    />
+    <!-- Data -->
+    <div slot="sensor">{tr('devices.state')} : {entity.state}</div>
+</EntityPreview>
