9b2fe1c9baccb09202e34a153e124c448538aee6
asoran <altan.soran@gmail.com>
2021-02-25 12:34:06 +0100

    feat(client): toast when a socket error happens

--- client/src/managers/socketManager.ts
+++ client/src/managers/socketManager.ts
@@ -1,6 +1,8 @@
 /* eslint-disable camelcase */
 import { io } from 'socket.io-client';
 import config from '../config/config';
+import { debounce } from '../utils/functions';
+import toastService from '../utils/toast';
 
 // export interface EntityDataChanged<T> {
 //     entity_id: string;
@@ -21,6 +23,11 @@ function isSocketError(data: any): data is SocketError {
     return 'error' in data;
 }
 
+// Need to fix the debounce function generic types...
+const toastError: (name: string|SocketError, data: string|SocketError) => void = debounce(
+    (name: string|SocketError, data: string|SocketError) => toastService.toast(`Error on event ${name}. Result: ${JSON.stringify(data)}`),
+);
+
 class SocketManager {
     private socket: any;
 
@@ -29,30 +36,46 @@ class SocketManager {
         console.log('Connected to WS');
     }
 
+    // Need these 2 maps because we need to store the function
+    // or else we can't remove the listener correctly
+    private funcToListenerById = new Map();
+
+    private funcToGlobalListener = new Map();
+
     registerListenerById<T>(name: string, id: string, func: (data: EntityDataChanged<T>) => void) {
-        this.socket.on(name, (data: EntityDataChanged<T> | SocketError) => {
+        const f = (data: EntityDataChanged<T> | SocketError) => {
             if (isSocketError(data)) {
-                console.log(`Error on event ${name}. Result: ${JSON.stringify(data)}`);
+                toastError(name, data);
                 return;
             }
             if ((data as EntityDataChanged<T>).id === id) {
                 func(data);
             }
-        });
+        };
+
+        this.funcToListenerById.set(func, f);
+        this.socket.on(name, f);
     }
 
-    registerGlobalListener(name: string, func: (data: any) => void) {
-        this.socket.on(name, (data: SocketError | any) => {
+    registerGlobalListener(name: string, func: (data: unknown) => void) {
+        const f = (data: SocketError | unknown) => {
             if (isSocketError(data)) {
-                console.log(`Error on event ${name}. Result: ${data}`);
+                toastError(name, data);
                 return;
             }
             func(data);
-        });
+        };
+
+        this.funcToGlobalListener.set(func, f);
+        this.socket.on(name, f);
     }
 
     removeListener<T>(name: string, func: (data: T) => void) {
-        this.socket.off(name, func);
+        if (this.funcToGlobalListener.has(func)) {
+            this.socket.off(name, this.funcToGlobalListener.get(func));
+        } else {
+            this.socket.off(name, this.funcToListenerById.get(func));
+        }
     }
 }
 
