5af4ad8a642efe3b607ace8312a78f6f190f579b
furtiveJack <tanguytraina@hotmail.fr>
2021-02-26 11:03:49 +0100

    feat(client): implement userService routes

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,3 +1,4 @@
+import type { User, UserWithoutId } from '../../types/userType';
 import http from '../utils/HttpHelper';
 
 export default class UserService {
@@ -11,5 +12,40 @@ export default class UserService {
 
     static async unlockFront(password: string): Promise<boolean> {
         return http.post('/user/unlock', { password });
+    }
+
+    static async getUsers(): Promise<User[]> {
+        return http.get('/user');
+    }
+
+    static async getUserById(id: string): Promise<User> {
+        return http.get(`/user/${id}`);
+    }
+
+    static async createUser(username: string, admin: boolean, entities?: string[]): Promise<User> {
+        const body: UserWithoutId = { username, admin };
+        if (entities) {
+            body.entities = entities;
+        }
+        return http.post('/user', body);
+    }
+
+    static async updateUser(id: string, username?: string,
+        admin?: boolean, entities?: string[]): Promise<User> {
+        const body: {
+            username?: string;
+            admin?: boolean;
+            entities?: string[];
+        } = {};
+        if (username) {
+            body.username = username;
+        }
+        if (admin) {
+            body.admin = admin;
+        }
+        if (entities) {
+            body.entities = entities;
+        }
+        return http.put(`/user/${id}`, body);
     }
 }
