80e3a7f01c9a93ef7cc1fc39f7825149527fb60a
Kylian <kbohl@etud.u-pem.fr>
2021-03-03 18:36:16 +0100

    fix(linter): Linter, please, get some AI

--- server/src/forwarders/index.ts
+++ server/src/forwarders/index.ts
@@ -1,4 +1,4 @@
+/* eslint-disable import/no-cycle */
 export { default as databaseForwarder } from './databaseForwarder';
 export { default as httpForwarder } from './httpForwarder';
-// eslint-disable-next-line import/no-cycle
 export { default as socketForwarder } from './socketForwarder';
